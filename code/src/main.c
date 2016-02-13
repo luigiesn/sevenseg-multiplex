79fb1e28c5404d3aa952ea8db3a5418cd64088a5
Luigi Sica Nery <luigiesn@users.noreply.github.com>
2016-02-13 12:25:36 -0200

    - Included some comments - Removed some useless lines

--- code/src/main.c
+++ code/src/main.c
@@ -1,6 +1,11 @@
 #include "../include/interrupt.h"
 #include "../include/sevenseg.h"
 
+/*
+ * sevenseg-multiplex code application example using timer interrupt to control
+ * the display changing interval time
+ */
+
 void main(void) {
     /* Config timer 0*/
     OPTION_REG &= ~0x20; /*  Internal instruction cycle clock */
@@ -10,21 +15,24 @@ void main(void) {
     INTCON &= ~0x04; /* Clean flag */
     INTCON |= 0x20; /* Enable TIMER0 interrupt */
 
-    TMR0 = 222; /* Preload */
-
     GIE = 1;
 
     /* Assing seven segments to timer0 interrupt*/
     interrupt_Assing(TMR0_INT_NUM, sevenseg_Multiplex);
 
+    /* Intialize displays*/
     sevenseg_Init();
+
+    /* Set values*/
     sevenseg_SetValue(0, 1, 0);
     sevenseg_SetValue(1, 2, 0);
     sevenseg_SetValue(2, 3, 0);
 
     for (;;) {
+        /* Don't need to do nothing in this case, timer0 interrupt routine
+         * do the all dirty job */
 
-
+        /* Stay here for()ever! */
     }
 
 }
